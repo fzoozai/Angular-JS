5f96c8ba61028516fe95503c800409a0171c4ef3
fzoozai <fabian.soosaithasan@gmail.com>
2016-10-01 15:05:41 +0200

    Add table with repos dep. on user

--- script.js
+++ script.js
@@ -7,22 +7,30 @@
         var app = angular.module("githubViewer", []);
 
         var MainCtrl = function ($scope, $http) {
+
             var onUserComplete = function (response) {
                 $scope.user = response.data;
+                $http.get($scope.user.repos_url)
+                    .then(onRepos, onError);
+            };
+
+            var onRepos = function (response) {
+                $scope.repos = response.data;
             }
+
             var onError = function (reason) {
-                $scope.error= "Could not fetch user! :("
-            }
+                $scope.error= "Could not fetch the data :("
+            };
 
             $scope.search = function(username){
                 $http.get("http://api.github.com/users/" + username)
                     .then(onUserComplete, onError);
-            }
+            };
 
             $http.get("http://api.github.com/users/angular")
-            .then(onUserComplete);
+                .then(onUserComplete);
 
-
+            /*Default values for the form*/
             $scope.username = "angular";
             $scope.message = "Github Viewer"
 
